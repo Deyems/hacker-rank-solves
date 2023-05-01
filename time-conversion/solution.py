8d9ed8c6b7a74f6e8d07aaa35eb0df0bd029ed3f
dzabeligan <balogun_elijah@outlook.com>
2023-05-01 02:20:48 +0100

    refactor: Modify solution

--- time-conversion/solution.py
+++ time-conversion/solution.py
@@ -12,15 +12,17 @@ import os
 
 def timeConversion(s):
     # Write your code here
-    sCopy = s[:-2]
+    hour = s[:2]
 
-    if s[-2:] == 'PM' and s[:2] != '12':
-        hour = (int(sCopy[:2]) + 12) % 24
-        sCopy = str(hour // 10) + str(hour % 10) + sCopy[2:]
-    elif s[-2:] == 'AM' and s[:2] == '12':
-        sCopy = '00' + sCopy[2:]
+    if s[-2:] == 'PM' and hour != '12':
+        # `hour + 12 < 24` as input time is valid, Constraint
+        # 01 <= hour <= 11. 12 is excluded in condition
+        # `hour + 12 > 10`, therefore, will not need padding with 0
+        hour = str(int(hour) + 12)
+    elif s[-2:] == 'AM' and hour == '12':
+        hour = '00'
 
-    return sCopy
+    return hour + s[2:-2]
 
 
 if __name__ == '__main__':
