8621d4e326a422853a2e910781818d9e5951062e
dzabeligan <balogun_elijah@outlook.com>
2023-05-05 16:51:49 +0100

    feat: Implement sparse arrays (solution 1)

---
+++ sparse_arrays/solution.py
@@ -0,0 +1,54 @@
+#!/usr/local/bin/python3
+
+import os
+
+#
+# Complete the 'matchingStrings' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. STRING_ARRAY strings
+#  2. STRING_ARRAY queries
+#
+
+
+def matchingStrings(strings, queries):
+    # O(n + q)
+    # Write your code here
+    string_dict = {}
+    result = []
+
+    for string in strings:
+        string_dict[string] = string_dict.get(string, 0) + 1
+
+    for query in queries:
+        result.append(string_dict.get(query, 0))
+
+    return result
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    strings_count = int(input().strip())
+
+    strings = []
+
+    for _ in range(strings_count):
+        strings_item = input()
+        strings.append(strings_item)
+
+    queries_count = int(input().strip())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = input()
+        queries.append(queries_item)
+
+    res = matchingStrings(strings, queries)
+
+    fptr.write('\n'.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
